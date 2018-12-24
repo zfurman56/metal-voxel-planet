f154653871ef6d0244d5d8a32e47821c1814867a
Zach Furman <zach.furman1@gmail.com>
2018-12-23 22:52:23 -0800

    Initial Commit

---
+++ Metal-EngineTests/Metal_EngineTests.swift
@@ -0,0 +1,36 @@
+//
+//  Metal_EngineTests.swift
+//  Metal-EngineTests
+//
+//  Created by Zach Furman on 12/23/18.
+//  Copyright © 2018 Zach Furman. All rights reserved.
+//
+
+import XCTest
+@testable import Metal_Engine
+
+class Metal_EngineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
